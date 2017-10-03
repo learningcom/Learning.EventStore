0584c870d13a73fb031370c933728feae3c8187a
Tom Mooney <tmooney@learning.com>
2017-10-03 12:08:34 -0700

    Gzip compress values before they go into event store. Add a condition to the save transaction to avoid collisions of commit Id

---
+++ src/Learning.EventStore/Extensions/StringExtensions.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Text;
+
+namespace Learning.EventStore.Extensions
+{
+    public static class StringExtensions
+    {
+        private const int MinCompressionSize = 1000;
+
+        public static string Compress(this string value)
+        {
+            if (Encoding.UTF8.GetByteCount(value) > MinCompressionSize && !value.IsCompressed())
+            {
+                var bytes = Encoding.UTF8.GetBytes(value);
+                using (var msi = new MemoryStream(bytes))
+                using (var mso = new MemoryStream())
+                {
+                    using (var gs = new GZipStream(mso, CompressionMode.Compress))
+                    {
+                        msi.CopyTo(gs);
+                    }
+
+                    var compressedString = Convert.ToBase64String(mso.ToArray());
+
+                    return compressedString;
+                }
+            }
+
+            return value;
+        }
+
+        public static string Decompress(this string value)
+        {
+            if (!value.IsCompressed())
+            {
+                return value;
+            }
+
+            try
+            {
+                var bytes = Convert.FromBase64String(value);
+                using (var msi = new MemoryStream(bytes))
+                using (var mso = new MemoryStream())
+                {
+                    using (var gs = new GZipStream(msi, CompressionMode.Decompress))
+                    {
+                        gs.CopyTo(mso);
+                    }
+
+                    return Encoding.UTF8.GetString(mso.ToArray());
+                }
+            }
+            catch (InvalidDataException)
+            {
+                return value;
+            }
+        }
+
+        public static bool IsCompressed(this string value)
+        {
+            //Check for the Base64 encoded Gzip header
+            const string gzipHeader = "H4sI";
+            return value.Substring(0, 4) == gzipHeader;
+        }
+    }
+}
--- src/Learning.EventStore/RedisEventStore.cs
+++ src/Learning.EventStore/RedisEventStore.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Learning.EventStore.Extensions;
 using Newtonsoft.Json;
 using StackExchange.Redis;
 
@@ -12,6 +13,7 @@ namespace Learning.EventStore
         private readonly IRedisClient _redis;
         private readonly IEventPublisher _publisher;
         private readonly string _keyPrefix;
+        private const int SaveRetryCount = 10;
 
         public RedisEventStore(IRedisClient redis, IEventPublisher publisher, string keyPrefix)
         {
@@ -29,7 +31,7 @@ namespace Learning.EventStore
             var settings = new JsonSerializerSettings { TypeNameHandling = TypeNameHandling.All };
 
             //Get the event data associated with each commit
-            var commitList = new List<RedisValue>();
+            var commitList = new List<string>();
             foreach (var commit in commits)
             {
                 var hashSetTask = _redis.HashGetAsync($"EventStore:{_keyPrefix}", commit).ConfigureAwait(false);
@@ -37,7 +39,7 @@ namespace Learning.EventStore
             }
 
             //Get the events that have happened since specified fromVersion
-            var events = commitList.Select(serializedEvent => JsonConvert.DeserializeObject<IEvent>(serializedEvent, settings))
+            var events = commitList.Select(serializedEvent => JsonConvert.DeserializeObject<IEvent>(serializedEvent.Decompress(), settings))
                                    .Where(x => x.Version > fromVersion)
                                    .ToList();
             return events;
@@ -51,30 +53,39 @@ namespace Learning.EventStore
             foreach (var @event in events)
             {
                 var serializedEvent = JsonConvert.SerializeObject(@event, settings);
+                var compressedEvent = serializedEvent.Compress();
 
-                //Create a Redis transaction
-                var tran = _redis.Database.CreateTransaction();
-
-                //Increment the commitId
-                var commitId = await _redis.HashLengthAsync(hashKey).ConfigureAwait(false) + 1;
-
-                //Write event data to a field named {commitId} in EventStore hash. Allows for fast lookup O(1) of individual events
-                var hashSetTask = tran.HashSetAsync(hashKey, commitId, serializedEvent).ConfigureAwait(false);
-
-                //Write the commitId to a list mapping commitIds to individual events for a particular aggregate (@event.Id)
-                var listPushTask = tran.ListRightPushAsync($"{{EventStore:{_keyPrefix}}}:{@event.Id}", commitId.ToString()).ConfigureAwait(false);
-
-                //Publish the event
-                var publishTask = _publisher.Publish(@event).ConfigureAwait(false);
-
-                //Execute the transaction
-                if (await tran.ExecuteAsync())
+                for (var i = 0; i < SaveRetryCount; i++)
                 {
-                    await hashSetTask;
-                    await listPushTask;
-                    await publishTask;
+                    //Create a Redis transaction
+                    var tran = _redis.Database.CreateTransaction();
+
+                    //Increment the commitId
+                    var commitId = await _redis.HashLengthAsync(hashKey).ConfigureAwait(false) + 1;
+
+                    //Ensure that the hash length has not been changed by another thread between now and when the transaction is committed to avoid commitId collisions
+                    tran.AddCondition(Condition.HashLengthEqual(hashKey, commitId));
+
+                    //Write event data to a field named {commitId} in EventStore hash. Allows for fast lookup O(1) of individual events
+                    var hashSetTask = tran.HashSetAsync(hashKey, commitId, compressedEvent).ConfigureAwait(false);
+
+                    //Write the commitId to a list mapping commitIds to individual events for a particular aggregate (@event.Id)
+                    var listPushTask = tran.ListRightPushAsync($"{{EventStore:{_keyPrefix}}}:{@event.Id}", commitId.ToString()).ConfigureAwait(false);
+
+                    //Publish the event
+                    var publishTask = _publisher.Publish(@event).ConfigureAwait(false);
+
+                    //Execute the transaction
+                    if (await tran.ExecuteAsync())
+                    {
+                        await hashSetTask;
+                        await listPushTask;
+                        await publishTask;
+                        return;
+                    }
                 }
-             
+
+                throw new InvalidOperationException(string.Format($"Failed to save value in key {hashKey}"));
             }
         }
     }
