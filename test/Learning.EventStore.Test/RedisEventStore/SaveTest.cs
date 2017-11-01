08eb61f226dbaaea662821ba1fdf72d1ff686b46
Tom Mooney <tmooney@learning.com>
2017-11-01 15:49:02 -0700

    fix unit test

--- test/Learning.EventStore.Test/RedisEventStore/SaveTest.cs
+++ test/Learning.EventStore.Test/RedisEventStore/SaveTest.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using FakeItEasy;
+using Learning.EventStore.Domain.Exceptions;
 using Learning.EventStore.Infrastructure;
 using Learning.EventStore.Test.Mocks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -112,11 +113,11 @@ namespace Learning.EventStore.Test.RedisEventStore
                 await _redisEventStore.SaveAsync(_eventList);
                 Assert.Fail("Should have thrown InvalidOperationException");
             }
-            catch(InvalidOperationException e)
+            catch(ConcurrencyException e)
             {
-                Assert.IsTrue(e.Message.Contains("Failed to save value in key EventStore:test:"));
+                Assert.AreEqual("A different version than expected was found in aggregate 12345", e.Message);
                 A.CallTo(() => _trans.PublishAsync("test:TestEvent", true, CommandFlags.None)).MustNotHaveHappened();
-                A.CallTo(() => _trans.ListRightPushAsync(A<RedisKey>.That.Matches(x => x.ToString().Contains(_eventList.First().Id)), A<RedisValue>._, When.Always, CommandFlags.None)).MustHaveHappened(Repeated.Exactly.Times(10));
+                A.CallTo(() => _trans.ListRightPushAsync(A<RedisKey>.That.Matches(x => x.ToString().Contains(_eventList.First().Id)), A<RedisValue>._, When.Always, CommandFlags.None)).MustHaveHappened();
                 A.CallTo(() => _redis.HashDeleteAsync(A<string>.That.Contains("EventStore:test"), A<string>._)).MustHaveHappened();
             }
         }
