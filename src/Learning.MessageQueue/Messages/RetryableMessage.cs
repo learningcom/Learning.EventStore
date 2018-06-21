32e3b86f24c08150d7cc740a5235b05d87d8172f
Tom Mooney <tmooney@learning.com>
2018-06-21 09:27:30 -0700

    ISO-556 Add RetryableEvent and RetryableMessage

---
+++ src/Learning.MessageQueue/Messages/RetryableMessage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Learning.EventStore.Common.Retry;
+
+namespace Learning.MessageQueue.Messages
+{
+    public abstract class RetryableMessage : IMessage, IRetryable
+    {
+        public string Id { get; set; }
+
+        public DateTimeOffset TimeStamp { get; set; }
+
+        public virtual int RetryForHours { get; set; } = 0;
+
+        public virtual int RetryLimit { get; set; } = 5;
+
+        public virtual int RetryIntervalMinutes { get; set; } = 5;
+
+        public virtual int RetryIntervalMaxMinutes { get; set; } = 60;
+    }
+}
